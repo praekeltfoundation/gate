76890dac9d63287d8c9c2c95a700bd2e8dd87f5d
Racter <paracetamolboy@gmail.com>
2018-01-22 16:18:16 +0200

    Import required class to read vaultToken value

--- gate-web/src/main/groovy/com/netflix/spinnaker/gate/controllers/SecretManagementController.groovy
+++ gate-web/src/main/groovy/com/netflix/spinnaker/gate/controllers/SecretManagementController.groovy
@@ -26,6 +26,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.beans.factory.annotation.Value
 import groovy.transform.CompileStatic
 import retrofit.RetrofitError
 import groovy.util.logging.Slf4j
